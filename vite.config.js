fff349552fdd6526161d0b4f73e9d8d9331c7e22
encento <encento@gmail.com>
2025-11-15 09:47:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/DzenGun-STE/",
   plugins: [react()],
-  base: "./",          // важное место — относительные пути
-  build: {
-    outDir: "docs",    // билд будет в /docs
-  },
 });
+
